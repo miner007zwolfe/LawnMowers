483bfef79f01ca4d9e5d5e20baf800cc3e607d4b
miner007zwolfe <126795867+miner007zwolfe@users.noreply.github.com>
2023-03-02 09:56:53 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function showTime() {
+
+}
+showTime();
+setInterval(function () {
+	showTime();
+}, 1000);
+
+if(document.getElementById('button').clicked == true)
+{
+   alert("button was clicked");
+}
